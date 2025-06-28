7df1196333e85a977060442a01a86d7baa313a66
aman-prasad1 <amanprasad048@gmail.com>
2025-06-28 21:48:53 +0530

    add order product and user models

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter Product name"],
+        trim: true
+    },
+    description: {
+        type: String,
+        required: [true, "Please enter Product description"]
+    },
+    price: {
+        type: Number,
+        required: [true, "Please enter Price"],
+        maxLength: [8, "Price cannot exceed 8 characters"]
+    },
+    ratings: {
+        type: Number,
+        default: 0
+    },
+    images: [
+        {
+            url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    category: {
+        type: String,
+        required: [true, "Please enter Product category"]
+    },
+    stock: {
+        type: Number,
+        default: 1
+    },
+    numberOfReviews: {
+        type: Number,
+        default: 0
+    },
+    reviews: [
+        {
+            user: {
+                type: mongoose.Schema.ObjectId,
+                ref: "User",
+                required: true
+            },
+            rating: {
+                type: Number,
+                required: true
+            },
+            comment: {
+                type: String,
+            }
+        }
+    ],
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+export const Product = mongoose.model("Product", productSchema);
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,84 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Please Enter Your Name"],
+        trim: true,
+        minLength: [3, "Name must contains atleast 2 characters"],
+        maxLength: [30, "Name cannot exceed 30 characters"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please Enter Your Email"],
+        unique: true,
+        trim: true,
+        index: true,
+        lowercase: true,
+        validate: [validator.isEmail, "Please Enter a valid Email"]
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: "user"
+    },
+    password: {
+        type: String,
+        required: [true, "Please Enter Your Password"],
+        minLength: [6, "Password must contains atleast 6 characters"]
+    },
+    refreshToken: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(this.password, password);
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            name: this.name
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema);
