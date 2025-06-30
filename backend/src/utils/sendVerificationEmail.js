57938b538390d03b7b9896fe1b0ed0a6dc392abf
aman-prasad1 <amanprasad048@gmail.com>
2025-06-30 23:20:32 +0530

    update verification email functionality

--- backend/src/utils/sendVerificationEmail.js
+++ backend/src/utils/sendVerificationEmail.js
@@ -1,13 +1,13 @@
 import { Resend } from "resend";
 
-export async function sendVerificationEmail(email, name, verifyCode) {
+export async function sendVerificationEmail(email, subject, message) {
     try {
         const resend = new Resend(process.env.RESEND_API_KEY)
         const response = await resend.emails.send({
             from: 'zentra@resend.dev',
             to: email,
-            subject: 'Zentra Verification Code',
-            html: `<strong>Hii ${name}. Your verification code: ${verifyCode}</strong>`,
+            subject: subject,
+            html: `<strong>${message}</strong>`,
         });
         if(!response.data) throw error;
         return {success: true, message: "Verification mail send successfully"};
