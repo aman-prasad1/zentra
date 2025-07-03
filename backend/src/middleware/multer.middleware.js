6f3d038d57fd595c76426edca60962de6774bccc
aman-prasad1 <amanprasad048@gmail.com>
2025-07-03 11:15:10 +0530

    unique name to uploaded file

--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -1,11 +1,15 @@
 import multer from 'multer';
+import path from 'path';
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "./public")
     },
     filename: function (req, file, cb) {
-        cb(null, new Date + file.originalname)
+        const ext = path.extname(file.originalname); // get extension (.jpg, .png)
+        const baseName = path.basename(file.originalname, ext); // original filename without extension
+        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+        cb(null, `${baseName}-${uniqueSuffix}${ext}`);
     }
 })
 
