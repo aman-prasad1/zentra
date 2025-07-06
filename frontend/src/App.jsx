5da64b1d5e94cf7b17a238c029f002fc6eb899ea
aman-prasad1 <amanprasad048@gmail.com>
2025-07-06 20:40:21 +0530

    add signup ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Loader from "./components/Loader";
+
+const SignUp = lazy(()=> import("./pages/SignUp.jsx"));
 
 const App = () => {
   return (
-    <div>
-      I'm On
-    </div>
+    <BrowserRouter>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/signup" element={<SignUp />}/>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   )
-}
+};
 
-export default App
+export default App;
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { GoEye, GoEyeClosed } from "react-icons/go";
+
+const SignUp = () => {
+  const [showCnfPass, setShowCnfPass] = useState(false);
+  const [showPass, setShowPass] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cnfPassword, setCnfPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  return (
+    <div className="h-screen w-screen flex justify-center items-center bg-amber-100">
+      <form className="w-11/12 h-9/12 p-4 sm:max-w-[500px] box-border flex flex-col gap-3 rounded-2xl shadow-2xl">
+        <h2 className="text-center text-3xl">SignUp</h2>
+
+        <div className="flex flex-col items-center gap-y-8 mt-5">
+          <input
+            type="text"
+            className="border-b w-10/12 h-10 p-3 outline-none hover:scale-105 transition-all font-light"
+            placeholder="Name"
+            spellCheck={false}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            className="border-b w-10/12 h-10 p-3 outline-none hover:scale-105 transition-all font-light"
+            placeholder="Email"
+            spellCheck={false}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <div className="border-b w-10/12 h-10 p-3 relative hover:scale-105 transition-all">
+            <input
+              type={`${showPass ? "text" : "password"}`}
+              className=" outline-none  font-light"
+              placeholder="Password"
+              spellCheck={false}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div
+              className="absolute top-3 end-3 cursor-pointer"
+              onClick={() => setShowPass(!showPass)}
+            >
+              {showPass ? <GoEye /> : <GoEyeClosed />}
+            </div>
+          </div>
+          <div className="border-b w-10/12 h-10 p-3 relative hover:scale-105 transition-all">
+            <input
+              type={`${showCnfPass ? "text" : "password"}`}
+              className={`outline-none font-light`}
+              placeholder="Confirm Password"
+              spellCheck={false}
+              onChange={(e) => setCnfPassword(e.target.value)}
+            />
+            <div
+              className="absolute top-3 end-3 cursor-pointer"
+              onClick={() => setShowCnfPass(!showCnfPass)}
+            >
+              {showCnfPass ? <GoEye /> : <GoEyeClosed />}
+            </div>
+            <span className="absolute left-1 top-10 text-red-700">/{errorMsg}</span>
+          </div>
+
+          <div className="w-full py-2 h-18 flex justify-center items-center box-border">
+            <button className="py-2 px-4 rounded bg-amber-500 cursor-pointer hover:text-lg transition-all">
+              Register
+            </button>
+          </div>
+        </div>
+        <span className="text-center font-light p-2">
+          Already have an account{" "}
+          <a href="/login" className="text-blue-700 font-bold underline">
+            Login
+          </a>
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
