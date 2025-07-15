90539034356350f4673f43e87d029a3a5f2d4da2
aman-prasad1 <amanprasad048@gmail.com>
2025-07-15 19:04:43 +0530

    add contact page

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { IoMailSharp } from "react-icons/io5";
 
 const ContactUs = () => {
   return (
-    <div>
-      Contact us
+    <div className='h-[80vh] flex justify-center items-center gap-10 text-4xl'>
+      <a target='_blank' href="https://github.com/aman-prasad1"><FaGithub /></a>
+      <a target='_blank' href="https://linkedin.com/in/amanprasad1"><FaLinkedin /></a>
+      <a target='_blank' href="https://instagram.com/aman_prasad88"><FaInstagram /></a>
+      <a target='_blank' href="mailto:amanprasad048@gmail.com"><IoMailSharp /></a>
     </div>
   )
 }
