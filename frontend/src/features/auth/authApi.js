4b3973199b75f732ff527200a89e2b5e1104a8d0
aman-prasad1 <amanprasad048@gmail.com>
2025-07-28 21:51:03 +0530

    add delete-account ui

--- frontend/src/features/auth/authApi.js
+++ frontend/src/features/auth/authApi.js
@@ -82,7 +82,7 @@ export const changePasswordApi = async (data) => {
 
 export const deleteAccountApi = async (password) => {
     try {
-        const res = await axios.delete(`${URL}/delete-profile`, password, {withCredentials: true});
+        const res = await axios.delete(`${URL}/delete-profile`, {data: {password}, withCredentials: true});
         return res.data;
     } catch (error) {
         const message = error?.response?.data?.message || "Profile not deleted";
