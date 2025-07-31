778a203e4141642945b21dca43ac6c48fd5c2115
aman-prasad1 <amanprasad048@gmail.com>
2025-07-31 21:44:09 +0530

    add admin feature slice

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,7 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import authSlice from '../features/auth/authSlice';
 import productSlice from '../features/product/productSlice';
 import cartSlice from '../features/cart/cartSlice';
-import orderSlice from '../features/order/orderSlice.js'
+import orderSlice from '../features/order/orderSlice.js';
+import adminSlice from '../features/admin/adminSlice.js';
 
 export const store = configureStore({
     reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
         productSlice,
         cartSlice,
         orderSlice,
+        adminSlice,
     },
 })
---
+++ frontend/src/features/admin/adminSlice.js
@@ -0,0 +1,15 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const adminSlice = createSlice(
+    {
+        name: 'admin',
+        initialState: {
+
+        },
+        reducers: {
+
+        }
+    }
+)
+
+export default adminSlice.reducer;
