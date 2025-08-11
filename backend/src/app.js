6a72563b9aa12d8d792bbff7ff5c1404d06a4d37
aman-prasad1 <amanprasad048@gmail.com>
2025-08-11 17:34:19 +0530

    fix: can't find public folder

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { errorMiddleware } from './middleware/error.middleware.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -31,7 +36,7 @@ app.use(cors({
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../../public")));
 app.use(cookieParser());
 
 // routes import
