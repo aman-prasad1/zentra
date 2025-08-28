5ae899862a0846e56124ec07c40ad17841e71743
Aman Prasad <amanprasad048@gmail.com>
2025-08-28 21:54:04 +0530

    fix: increase tokens expirydate

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -155,7 +155,7 @@ const loginUser = asyncHandler(async (req, res) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-        maxAge: 24 * 60 * 60 * 1000, // 1 day
+        maxAge: 24 * 60 * 60 * 1000 * 10, // 10 day
         path: '/'
     }
 
